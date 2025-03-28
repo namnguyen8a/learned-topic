5e6923d83665eb4eae068c59c6fe63b11e220640
namnguyen <namnguyen.8a@gmail.com>
2025-03-28 18:48:38 +0700

    adding basig-rag implement

--- RAG/basic-rag/main.py
+++ RAG/basic-rag/main.py
@@ -1,1 +1,7 @@
-... 
+from google import genai
+
+client = genai.Client(api_key="AIzaSyAtDmczb6fFqMoquokFyvViLMG_jEUyKkE")
+response = client.models.generate_content(
+    model="gemini-2.0-flash", contents="Explain how AI works"
+)
+print(response.text)
